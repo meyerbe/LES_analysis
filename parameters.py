5b2991c41e8cdf2af5a3cd452288d2ff9933c8f5
Bettina Meyer <bettina.meyer@erdw.ethz.ch>
2016-11-23 10:19:57 +0100

    LES parameters

---
+++ parameters.py
@@ -0,0 +1,34 @@
+# //Generated code! Absolutely DO NOT modify this file, parameters should be modified in generate_parameters.py
+def pi(): 3.14159265359
+# define g 9.80665
+global Rd, Rv
+Rd = 287.1
+Rv = 461.5
+global eps_v, eps_vi
+eps_v = 0.62210184182
+eps_vi = 1.60745384883
+global cpd, cpv, cl, ci
+cpd = 1004.0
+cpv = 1859.0
+cl = 4218.0
+ci = 2106.0
+# define kappa 0.285956175299
+# define Tf 273.15
+# define Tt 273.16
+global T_tilde, p_tilde, sd_tilde, sv_tilde
+T_tilde = 298.15
+p_tilde = 100000.0
+# define pv_star_t 611.7
+sd_tilde = 6864.8
+sv_tilde = 10513.6
+# define omega 7.29211514671e-05
+# define ql_threshold 1e-08
+# define vkb 0.35
+# define Pr0 0.74
+# define beta_m 4.7
+# define beta_h 6.35135135135
+# define gamma_m 15.0
+# define gamma_h 9.0
+
+
+# //End generated code
