5e86ce91790254eb2814cc0daa10d36e2fb35830
Bettina Meyer <bettina.meyer@erdw.ethz.ch>
2017-06-08 23:16:41 +0200

    Stats_cond: modifying error plotting

--- Stats_cond/main_PDF_cond.py
+++ Stats_cond/main_PDF_cond.py
@@ -144,14 +144,14 @@ def set_zrange(case_name):
         # files = ['21600.nc']
         ## Bomex (dz=20)
         # krange = np.asarray([50, 60], dtype=np.int32)
-        # krange = np.asarray([15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 100, 125], dtype=np.int32)
-        # files = ['18000.nc', '19800.nc', '21600.nc']
+        krange = np.asarray([15, 20, 25, 30, 35, 40, 50, 60, 70, 80, 90, 100, 125], dtype=np.int32)
+        files = ['18000.nc', '19800.nc', '21600.nc']
         # files = ['21600.nc']
         # Bomex test
-        files = ['21600.nc']
+        # files = ['21600.nc']
         # krange = np.asarray([10, 17, 20, 25, 50])
         # krange = np.asarray([10, 12, 15, 18, 20, 22, 25, 40, 50])
-        krange = np.asarray([20, 50], dtype=np.int32)
+        # krange = np.asarray([20, 50], dtype=np.int32)
         # krange = np.asarray([18,30,38])
     elif case_name == 'TRMM_LBA':
         # TRMM
--- Stats_cond/plotting_errors_alltime.py
+++ Stats_cond/plotting_errors_alltime.py
@@ -25,34 +25,17 @@ def main():
     parser.add_argument("casename")
     # parser.add_argument("--file_name")
     parser.add_argument("time")
-    parser.add_argument("Lx")
-    parser.add_argument("dz")
+    parser.add_argument("Lx", nargs='+', type=int)
+    parser.add_argument("--dz", nargs='+', type=int)
     # parser.add_argument("time")
     parser.add_argument("--xa_ql")
     parser.add_argument("--xb_ql")
+    parser.add_argument("--xa_cf")
+    parser.add_argument("--xb_cf")
 
     args = parser.parse_args()
     path = args.path
     case_name = args.casename
-    time_field = np.int(args.time)
-    Lx = np.double(args.Lx)
-    delta_z = np.int(args.dz)
-    time_name = '5to6h'
-
-    type_ = 'Couvreux'
-    # file_name = args.file_name
-    # file_name = 'PDF_cond_alltime_res_error_Lx' + str(np.round(Lx,1)) + 'Ly' + str(np.round(Lx,1)) \
-    #             + '_dz' + str(np.int(delta_z)) + '_time' + str(time) + '.nc'
-    file_name = 'PDF_cond_alltime_' + type_ + '_allcomp_Lx' + str(np.round(Lx, 1)) + 'Ly' + str(np.round(Lx, 1)) \
-                + '_dz' + str(np.int(delta_z)) + '_time' + str(time_field) + '.nc'
-    print('')
-    print('path:', path)
-    print('file name: ', file_name)
-    print('')
-
-    fullpath = os.path.join(path, file_name)
-
-    path_ref = os.path.join(path, 'Stats.' + case_name + '.nc')
 
     nml = simplejson.loads(open(os.path.join(path, case_name + '.in')).read())
     nz = nml['grid']['nz']
@@ -60,70 +43,110 @@ def main():
     dt_stats = nml['stats_io']['frequency']
     t_ref = 0
 
+    time_field = np.int(args.time)
+    Lx_range = args.Lx
+    if args.dz:
+        delta_z_range = args.dz
+    else:
+        delta_z_range = [dz]
+    time_name = '5to6h'
+    print('Lx range: ', Lx_range)
+    print('dz range: ', delta_z_range)
 
-    path_in = os.path.join(path, 'PDF_cond', file_name)
-    path_out = os.path.join(path, 'PDF_cond_figures', 'error_profiles')
-    print(path_in)
-    rootgrp = nc.Dataset(path_in, 'r')
-    zrange = rootgrp.groups['profiles'].variables['zrange'][:]
-    error_ql_env = rootgrp.groups['error'].variables['error_ql_env'][:, :]
-    error_ql_rel_env = rootgrp.groups['error'].variables['rel_error_ql_env'][:, :]
-    ql_mean_env = rootgrp.groups['profiles'].variables['ql_mean_env'][:]
-    ql_mean_domain = rootgrp.groups['profiles'].variables['ql_mean_domain'][:]
-    error_cf_env = rootgrp.groups['error'].variables['error_cf_env'][:, :]
-    error_cf_rel_env = rootgrp.groups['error'].variables['rel_error_cf_env'][:, :]
-    cf_env = rootgrp.groups['profiles'].variables['cf_env'][:]
-    cf_domain = rootgrp.groups['profiles'].variables['cf_domain'][:]
-    rootgrp.close()
+    type_ = 'Couvreux'
+    for Lx_ in Lx_range:
+        Lx = np.double(Lx_)
+        for delta_z in delta_z_range:
+            # file_name = args.file_name
+            # file_name = 'PDF_cond_alltime_res_error_Lx' + str(np.round(Lx,1)) + 'Ly' + str(np.round(Lx,1)) \
+            #             + '_dz' + str(np.int(delta_z)) + '_time' + str(time) + '.nc'
 
-    xa = -2.5e-6
-    xb = 2.5e-6
-    if args.xa_ql:
-        xa = np.double(args.xa_ql)
-    if args.xb_ql:
-        xb = np.double(args.xb_ql)
-    xlimits_ql = [xa, xb]
-    xlimits_cf = [-2e-2, 1e-1]
-    for ncomp_max in [3,5,8]:
-        plot_error_ql_ncompmax(error_ql_env, ql_mean_env, error_ql_rel_env, zrange,
-                               ncomp_max, xlimits_ql, dz, Lx, delta_z, time_name, t_ref, type_, path_out)
-        plot_error_cf_ncompmax(error_cf_env, cf_env, error_cf_rel_env, zrange,
-                               ncomp_max, xlimits_cf, dz, Lx, delta_z, time_name, t_ref, type_, path_out)
+            file_name = 'PDF_cond_alltime_' + type_ + '_allcomp_Lx' + str(np.round(Lx, 1)) + 'Ly' + str(np.round(Lx, 1)) \
+                    + '_dz' + str(np.int(delta_z)) + '_time' + str(time_field) + '.nc'
+            print('')
+            print('path:', path)
+            print('file name: ', file_name)
+            print('')
 
-    path_profile = os.path.join(path, 'Stats.'+case_name+'.nc')
-    print('')
-    print(path_profile)
-    print('')
-    root = nc.Dataset(path_profile, 'r')
-    cf_stats_ = root.groups['profiles'].variables['cloud_fraction'][:,:]
-    ql_stats_ = root.groups['profiles'].variables['ql_mean'][:,:]
-    time_stats = root.groups['profiles'].variables['t'][:]
-    zrange_stats = root.groups['profiles'].variables['z'][:]
-    it = np.int(time_field / dt_stats)
-    print('')
-    print('Times: ', time_field, time_stats[it])
-    print(time_stats.shape, it)
-    krange = np.zeros(shape=zrange.shape)
-    ql_stats = np.zeros(shape=zrange.shape)
-    cf_stats = np.zeros(shape=zrange.shape)
-    k_count = 0
-    k = 0
-    while(k_count < zrange.shape[0] and k < zrange_stats.shape[0]):
-        # for k in range(zrange_stats.shape[0]):
-        if zrange_stats[k] == zrange[k_count]:
-            # print('k', k, k_count)
-            # print(zrange_stats[k], zrange[k_count])
-            # print('')
-            krange[k_count] = k
-            ql_stats[k_count] = ql_stats_[it, k]
-            cf_stats[k_count] = cf_stats_[it, k]
-            k_count += 1
-        k += 1
-    # print('krange: ', krange)
-    print('')
+            fullpath = os.path.join(path, file_name)
 
-    plot_mean_profiles(ql_mean_env, ql_mean_domain, ql_stats, cf_env, cf_domain, cf_stats,
-                       Lx, zrange, time_name, type_, path_out)
+            path_ref = os.path.join(path, 'Stats.' + case_name + '.nc')
+
+
+
+
+            # path_in = os.path.join(path, 'PDF_cond', file_name)
+            # path_out = os.path.join(path, 'PDF_cond_figures', 'error_profiles')
+            path_in = os.path.join(path, 'PDF_cond_anomaly', file_name)
+            path_out = os.path.join(path, 'PDF_cond_anomaly_figures', 'error_profiles')
+            print(path_in)
+            rootgrp = nc.Dataset(path_in, 'r')
+            zrange = rootgrp.groups['profiles'].variables['zrange'][:]
+            error_ql_env = rootgrp.groups['error'].variables['error_ql_env'][:, :]
+            error_ql_rel_env = rootgrp.groups['error'].variables['rel_error_ql_env'][:, :]
+            ql_mean_env = rootgrp.groups['profiles'].variables['ql_mean_env'][:]
+            ql_mean_domain = rootgrp.groups['profiles'].variables['ql_mean_domain'][:]
+            error_cf_env = rootgrp.groups['error'].variables['error_cf_env'][:, :]
+            error_cf_rel_env = rootgrp.groups['error'].variables['rel_error_cf_env'][:, :]
+            cf_env = rootgrp.groups['profiles'].variables['cf_env'][:]
+            cf_domain = rootgrp.groups['profiles'].variables['cf_domain'][:]
+            rootgrp.close()
+
+            xa = -2.5e-6
+            xb = 2.5e-6
+            if args.xa_ql:
+                xa = np.double(args.xa_ql)
+            if args.xb_ql:
+                xb = np.double(args.xb_ql)
+            xlimits_ql = [xa, xb]
+
+            xa = -5e-2
+            xb = 1e-1
+            if args.xa_cf:
+                xa = np.double(args.xa_cf)
+            if args.xb_cf:
+                xb = np.double(args.xb_cf)
+            xlimits_cf = [xa, xb]
+            for ncomp_max in [3,5,8]:
+                plot_error_ql_ncompmax(error_ql_env, ql_mean_env, error_ql_rel_env, zrange,
+                                       ncomp_max, xlimits_ql, dz, Lx, delta_z, time_name, t_ref, type_, path_out)
+                plot_error_cf_ncompmax(error_cf_env, cf_env, error_cf_rel_env, zrange,
+                                       ncomp_max, xlimits_cf, dz, Lx, delta_z, time_name, t_ref, type_, path_out)
+
+            path_profile = os.path.join(path, 'Stats.'+case_name+'.nc')
+            print('')
+            print(path_profile)
+            print('')
+            root = nc.Dataset(path_profile, 'r')
+            cf_stats_ = root.groups['profiles'].variables['cloud_fraction'][:,:]
+            ql_stats_ = root.groups['profiles'].variables['ql_mean'][:,:]
+            time_stats = root.groups['profiles'].variables['t'][:]
+            zrange_stats = root.groups['profiles'].variables['z'][:]
+            it = np.int(time_field / dt_stats)
+            print('')
+            print('Times: ', time_field, time_stats[it])
+            print(time_stats.shape, it)
+            krange = np.zeros(shape=zrange.shape)
+            ql_stats = np.zeros(shape=zrange.shape)
+            cf_stats = np.zeros(shape=zrange.shape)
+            k_count = 0
+            k = 0
+            while(k_count < zrange.shape[0] and k < zrange_stats.shape[0]):
+                # for k in range(zrange_stats.shape[0]):
+                if zrange_stats[k] == zrange[k_count]:
+                    # print('k', k, k_count)
+                    # print(zrange_stats[k], zrange[k_count])
+                    # print('')
+                    krange[k_count] = k
+                    ql_stats[k_count] = ql_stats_[it, k]
+                    cf_stats[k_count] = cf_stats_[it, k]
+                    k_count += 1
+                k += 1
+            # print('krange: ', krange)
+            print('')
+
+            plot_mean_profiles(ql_mean_env, ql_mean_domain, ql_stats, cf_env, cf_domain, cf_stats,
+                               Lx, zrange, time_name, type_, path_out)
 
 
 
@@ -203,7 +226,7 @@ def plot_error_cf_ncompmax(error_cf_env, cf_env, error_cf_rel_env, zrange,
         col = cm1(np.double(nc) / ncomp_max)
         plt.plot(error_cf_env[:, nc], zrange[:], '-x', color=col, label='ncomp=' + str(nc + 1))
     plt.plot(-cf_env[:], zrange[:], 'k--', label='- CF env.')
-    plt.legend(loc=2)
+    plt.legend(loc=1)
     plt.xlim(xlimits)
     plt.title('error CF environment (Lx=' + str(Lx) + ', dz=' + str(delta_z) + 'm, t=' + str(time) + ')')
     plt.xlabel(r'$\epsilon(CF)$')
@@ -212,7 +235,7 @@ def plot_error_cf_ncompmax(error_cf_env, cf_env, error_cf_rel_env, zrange,
     for nc in range(ncomp_max):
         col = cm1(np.double(nc) / ncomp_max)
         plt.plot(error_cf_rel_env[:, nc], zrange[:], '-x', color=col, label='ncomp=' + str(nc + 1))
-    plt.xlim([-1.1,2])
+    plt.xlim([-1.1,3])
     plt.legend()
     plt.title('relative error CF environment (Lx=' + str(Lx) + ', dz=' + str(delta_z) + 'm)')
     plt.xlabel(r'$\epsilon(CF)/CF$')
